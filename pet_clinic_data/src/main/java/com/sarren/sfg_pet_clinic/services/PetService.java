abf1c3b61fb370c497d5b6ec39ff4bb1c4c46c34
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-02 14:51:19 +0100

    Implement Interfaces for services. Closes #5

---
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.sarren.sfg_pet_clinic.services;
+
+import com.sarren.sfg_pet_clinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
