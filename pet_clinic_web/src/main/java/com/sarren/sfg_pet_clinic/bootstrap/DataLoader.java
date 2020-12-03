b525c982de80ccb1ba390fe616322c59efa35ec7
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-03 15:05:14 +0100

    Refactored Map Impl to abstrac ID Closes #23

--- pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/bootstrap/DataLoader.java
+++ pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/bootstrap/DataLoader.java
@@ -22,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setName("Sarren");
         owner1.setLastName("Abbeloos");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setName("Yana");
         owner2.setLastName("Kiekens");
         ownerService.save(owner2);
@@ -36,13 +34,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners ...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setName("Sam");
         vet1.setLastName("Axe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setName("Frodo");
         vet2.setLastName("Baggins");
         vetService.save(vet2);
