3d145d0a03467728c248aa75431a9bc2a05aeed1
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-04 15:45:22 +0100

    Added Pets and Contact info to owners on startup. Closes #33

--- pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/bootstrap/DataLoader.java
+++ pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.sarren.sfg_pet_clinic.bootstrap;
 
 import com.sarren.sfg_pet_clinic.model.Owner;
+import com.sarren.sfg_pet_clinic.model.Pet;
 import com.sarren.sfg_pet_clinic.model.PetType;
 import com.sarren.sfg_pet_clinic.model.Vet;
 import com.sarren.sfg_pet_clinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.sarren.sfg_pet_clinic.services.PetTypeService;
 import com.sarren.sfg_pet_clinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -33,15 +36,36 @@ public class DataLoader implements CommandLineRunner {
         dog.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+
         Owner owner1 = new Owner();
         owner1.setName("Sarren");
         owner1.setLastName("Abbeloos");
+        owner1.setAddress("Langevelde 67");
+        owner1.setCity("Merchtem");
+        owner1.setTelephone("052 37 30 38");
         ownerService.save(owner1);
+
+        Pet sarrensPet = new Pet();
+        sarrensPet.setPetType(savedDogPetType);
+        sarrensPet.setOwner(owner1);
+        sarrensPet.setBirthDate(LocalDate.now());
+        sarrensPet.setName("frie");
+        owner1.getPets().add(sarrensPet);
 
         Owner owner2 = new Owner();
         owner2.setName("Yana");
         owner2.setLastName("Kiekens");
+        owner2.setAddress("Langevelde 67");
+        owner2.setCity("Merchtem");
+        owner2.setTelephone("052 37 30 38");
         ownerService.save(owner2);
+
+        Pet yanasPet = new Pet();
+        yanasPet.setPetType(savedCatPetType);
+        yanasPet.setOwner(owner2);
+        yanasPet.setBirthDate(LocalDate.now());
+        yanasPet.setName("Fons");
+        owner2.getPets().add(yanasPet);
 
         System.out.println("Loaded owners ...");
 
