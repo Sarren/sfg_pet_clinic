f7e0ee6b8fb04d134a8f71ab92c690c790b0fae5
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-04 15:55:23 +0100

    Added specialties Map Impl. Closes #31

---
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.sarren.sfg_pet_clinic.services;
+
+import com.sarren.sfg_pet_clinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package com.sarren.sfg_pet_clinic.services.map;
+
+import com.sarren.sfg_pet_clinic.model.Specialty;
+import com.sarren.sfg_pet_clinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
