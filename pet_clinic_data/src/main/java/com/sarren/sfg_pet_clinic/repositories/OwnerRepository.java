7d3b9c772d0f9cc02b999b107e34190a292c3d05
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-07 16:42:47 +0100

    added spring data jpa service for owners. Closes #40

--- pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/repositories/OwnerRepository.java
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.sarren.sfg_pet_clinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
--- pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/repositories/VisitRepository.java
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/repositories/VisitRepository.java
@@ -1,6 +1,7 @@
 package com.sarren.sfg_pet_clinic.repositories;
 
+import com.sarren.sfg_pet_clinic.model.Visit;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VisitRepository extends CrudRepository<VisitRepository, Long> {
+public interface VisitRepository extends CrudRepository<Visit, Long> {
 }
