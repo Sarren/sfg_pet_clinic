271a115b2c621bc6034bb9d7560cad95b51cd44e
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-08 17:48:13 +0100

    added jpa test for owner service. Closes #50

---
+++ pet_clinic_data/src/test/java/com/sarren/sfg_pet_clinic/services/springdatajpa/OwnerSpringDataJpaServiceTest.java
@@ -0,0 +1,119 @@
+package com.sarren.sfg_pet_clinic.services.springdatajpa;
+
+import com.sarren.sfg_pet_clinic.model.Owner;
+import com.sarren.sfg_pet_clinic.repositories.OwnerRepository;
+import com.sarren.sfg_pet_clinic.repositories.PetRepository;
+import com.sarren.sfg_pet_clinic.repositories.PetTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerSpringDataJpaServiceTest {
+
+    public static final String LAST_NAME = "Abbeloos";
+    @Mock
+    OwnerRepository ownerRepository;
+
+    @Mock
+    PetRepository petRepository;
+
+    @Mock
+    PetTypeRepository petTypeRepository;
+
+    @InjectMocks
+    OwnerSpringDataJpaService service;
+
+    Owner returnOwner;
+
+    @BeforeEach
+    void setUp() {
+        returnOwner = Owner.builder().id(1L).lastName(LAST_NAME).build();
+    }
+
+    @Test
+    void findByLastName() {
+         // When findbyLastname is called on ownerrepository is called I want it to return a returnOwner
+        when(ownerRepository.findByLastName(any())).thenReturn(returnOwner);
+
+        Owner abbeloos = service.findByLastName(LAST_NAME);
+
+        assertEquals(LAST_NAME, abbeloos.getLastName());
+
+        //redundant
+        verify(ownerRepository).findByLastName(any());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> returnOwnerSet = new HashSet<>();
+        returnOwnerSet.add(Owner.builder().id(1L).build());
+        returnOwnerSet.add(Owner.builder().id(2L).build());
+
+        // mockito when
+        when(ownerRepository.findAll()).thenReturn(returnOwnerSet);
+
+        Set<Owner> owners = service.findAll();
+
+        assertNotNull(owners);
+        assertEquals(2, owners.size());
+    }
+
+    @Test
+    void findById() {
+        when(ownerRepository.findById(anyLong())).thenReturn(Optional.of(returnOwner));
+
+        Owner owner = service.findById(1L);
+
+        assertNotNull(owner);
+    }
+
+    @Test
+    void findByIdNotFound() {
+        when(ownerRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Owner owner = service.findById(1L);
+
+        assertNull(owner);
+    }
+
+    @Test
+    void save() {
+        Owner  ownerToSave = Owner.builder().id(1L).build();
+
+        when(ownerRepository.save(any())).thenReturn(returnOwner);
+
+        Owner savedOwner = service.save(ownerToSave);
+
+        assertNotNull(savedOwner);
+    }
+
+    @Test
+    void delete() {
+
+        service.delete(returnOwner);
+
+        // verify the delete method is called 1 time
+        //Good use in testing void methods, default is 1 times
+        verify(ownerRepository, times(1)).delete(any());
+    }
+
+    @Test
+    void deleteById() {
+        service.deleteById(1L);
+        verify(ownerRepository, times(1)).deleteById(anyLong());
+
+    }
+}
