1b4a751dcdd160f02f8f1b3a9e0ebb99eb9a1ce7
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-08 11:00:55 +0100

    Added Project Lombok and refactor. Closes #51

--- pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/model/Person.java
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/model/Person.java
@@ -1,30 +1,29 @@
 package com.sarren.sfg_pet_clinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
+
+    public Person(Long id, String name, String lastName) {
+        super(id);
+        this.name = name;
+        this.lastName = lastName;
+    }
 
     @Column(name = "first_name")
     private String name;
 
     @Column(name = "last_name")
     private String lastName;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
--- pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/model/Specialty.java
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/model/Specialty.java
@@ -1,21 +1,20 @@
 package com.sarren.sfg_pet_clinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "Specialties")
 public class Specialty extends BaseEntity{
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
