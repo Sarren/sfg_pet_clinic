d9c372c1fa75848dd7780abc0f5119e313b020f0
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-14 10:42:46 +0100

    Create or update Pet not working. #56

--- pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/model/Pet.java
+++ pet_clinic_data/src/main/java/com/sarren/sfg_pet_clinic/model/Pet.java
@@ -16,6 +16,15 @@ import java.util.Set;
 @Table(name = "Pets")
 public class Pet extends BaseEntity {
 
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
+
     @Column(name = "name")
     private String name;
 
