94370f75273b6cf2598a03e96d80369052ac5794
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-14 13:56:31 +0100

    Added visit Controller and Mappings. #57

---
+++ pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,38 @@
+package com.sarren.sfg_pet_clinic.formatters;
+
+import com.sarren.sfg_pet_clinic.model.PetType;
+import com.sarren.sfg_pet_clinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
