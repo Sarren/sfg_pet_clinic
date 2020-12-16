d590cd69dec8ff81697351cfe9e318a9b6f47f42
2020dev064 <63107015+2020dev064@users.noreply.github.com>
2020-12-16 10:40:47 +0100

    Added Json endpoint for vets. #66

--- pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/controllers/VetController.java
+++ pet_clinic_web/src/main/java/com/sarren/sfg_pet_clinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.sarren.sfg_pet_clinic.controllers;
 
+import com.sarren.sfg_pet_clinic.model.Vet;
 import com.sarren.sfg_pet_clinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
